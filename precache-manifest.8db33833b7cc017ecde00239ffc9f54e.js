62d320de1bf18015fff8f7a1a917a900adc26592
Nick Wang <nickyiliwang@gmail.com>
2019-11-10 23:54:10 -0500

    Updates

---
+++ precache-manifest.8db33833b7cc017ecde00239ffc9f54e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064a9580fffd7688de78660bdc1ca7ea",
+    "url": "/facial-recognition/index.html"
+  },
+  {
+    "revision": "478e27e42a5685935538",
+    "url": "/facial-recognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2ac332fab03dd76e10d3",
+    "url": "/facial-recognition/static/css/main.f1782b83.chunk.css"
+  },
+  {
+    "revision": "478e27e42a5685935538",
+    "url": "/facial-recognition/static/js/2.6fa714f8.chunk.js"
+  },
+  {
+    "revision": "2ac332fab03dd76e10d3",
+    "url": "/facial-recognition/static/js/main.dd7af027.chunk.js"
+  },
+  {
+    "revision": "0d328eac97f6dc46f87d",
+    "url": "/facial-recognition/static/js/runtime~main.39db5ceb.js"
+  },
+  {
+    "revision": "7ae9c5eed534a12b90003b68f7b2f2c8",
+    "url": "/facial-recognition/static/media/face.7ae9c5ee.svg"
+  }
+]);
